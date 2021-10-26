e7e8e383cfbe6b6175d9373a853b189cbc4fadd8
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2021-10-26 23:33:56 +0530

    Task 1A E-Yantra

--- task_1a.py
+++ task_1a.py
@@ -36,59 +36,119 @@ import os
 
 ##############################################################
 
-def detect_shapes(img, ):
+def detect_shapes(img):
+	"""
+	Purpose:
+	---
+	This function takes the image as an argument and returns a nested list
+	containing details of colored (non-white) shapes in that image
 
-	detected_shapes = []
+	Input Arguments:
+	---
+	`img` :	[ numpy array ]
+			numpy array of image returned by cv2 library
 
+	Returns:
+	---
+	`detected_shapes` : [ list ]
+			nested list containing details of colored (non-white) 
+			shapes present in image
+
+	Example call:
+	---
+	shapes = detect_shapes(img)
+	"""
+	
 	##############	ADD YOUR CODE HERE	##############
-	# lower = {'red': ([166, 84, 141]), 'green': ([50, 50, 120]), 'blue': ([97, 100, 117]), 'yellow': ([23, 59, 119]),'orange': ([0, 50, 80]), 'purple': ([130, 80, 80])}
-	# upper = {'red': ([186, 255, 255]), 'green': ([70, 255, 255]), 'blue': ([117, 255, 255]), 'yellow': ([54, 255, 255]),'orange': ([20, 255, 255]), 'purple': ([150, 255, 255])}
-	# colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0), 'yellow': (0, 255, 217),'orange': (0, 140, 255), 'purple': (211, 0, 148)}
+	detected_shapes = []
+	lower = {'red': ([166, 84, 141]), 'green': ([50, 50, 120]), 'blue': ([80, 10, 0]), 'yellow': ([23, 59, 119]),
+			 'orange': ([0, 50, 80]), 'purple': ([130, 80, 80])}  # assign new item lower['blue'] = (93, 10, 0)
+	upper = {'red': ([186, 255, 255]), 'green': ([70, 255, 255]), 'blue': ([128, 255, 255]), 'yellow': ([54, 255, 255]),
+			 'orange': ([20, 255, 255]), 'purple': ([150, 255, 255])}
 
-	imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 	blurred = cv2.GaussianBlur(img, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-	_, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
-	contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	mlist = []
+	clist = []
+	ks = []
 
+	for (key, value) in upper.items():
 
-	for contour in contours:
-		ks = []
-		approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-		# cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
-		M = cv2.moments(contour)
-		if len(approx) == 3:
-			objectType = 'Triangle'
-		elif len(approx) == 4:
-			x1, y1, w, h = cv2.boundingRect(approx)
+		kernel = np.ones((2, 2), np.uint8)
+		mask = cv2.inRange(hsv, np.array(lower[key]), np.array(upper[key]))
+		mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+		mask = cv2.dilate(mask, kernel, iterations=1)
+		mlist.append(mask)
+		cnts, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		if len(cnts) >= 1:
+			clist.append(cnts[-1])
+			ks.append(key)
+
+	for i, cnt in enumerate(clist):
+		kg = []
+		peri = cv2.arcLength(cnt, True)
+		vertices = cv2.approxPolyDP(cnt, 0.04 * peri, True)
+
+		M = cv2.moments(cnt)
+
+		if len(vertices) == 3:
+			objectType = ' Triangle'
+			obj=ks[i]
+		elif len(vertices) == 4:
+			x1, y1, w, h = cv2.boundingRect(vertices)
 			aspectRatio = float(w) / h
-			print(aspectRatio)
 			if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-				objectType = 'square'
+				objectType = 'Square'
+				obj = ks[i]
 			else:
-				objectType = 'rectangle'
-		elif len(approx) == 5:
+				objectType = 'Rectangle'
+				obj = ks[i]
+		elif len(vertices) == 5:
 			objectType = 'Pentagon'
+			obj = ks[i]
 		else:
 			objectType = 'Circle'
+			obj = ks[i]
 
-		ks.append("green")
-		ks.append(objectType)
+		kg.append(obj)
+		kg.append(objectType)
 		cX = int(M['m10'] / M['m00'])
 		cY = int(M['m01'] / M['m00'])
-		gg=(cX,cY)
-		ks.append(gg)
-
-		detected_shapes.append(ks)
-
+		gg = (cX, cY)
+		kg.append(gg)
+		detected_shapes.append(kg)
 	##################################################
-	
+
 	return detected_shapes
 
 def get_labeled_image(img, detected_shapes):
 	######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS FUNCTION #########
+	"""
+	Purpose:
+	---
+	This function takes the image and the detected shapes list as an argument
+	and returns a labelled image
 
-	######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS FUNCTION #########    
+	Input Arguments:
+	---
+	`img` :	[ numpy array ]
+			numpy array of image returned by cv2 library
+
+	`detected_shapes` : [ list ]
+			nested list containing details of colored (non-white) 
+			shapes present in image
+
+	Returns:
+	---
+	`img` :	[ numpy array ]
+			labelled image
+
+	Example call:
+	---
+	img = get_labeled_image(img, detected_shapes)
+	"""
+	######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS FUNCTION #########
 
 	for detected in detected_shapes:
 		colour = detected[0]
@@ -98,49 +158,49 @@ def get_labeled_image(img, detected_shapes):
 	return img
 
 if __name__ == '__main__':
-	
+
 	# path directory of images in 'test_images' folder
 	img_dir_path = 'test_images/'
 
 	# path to 'test_image_1.png' image file
 	file_num = 1
 	img_file_path = img_dir_path + 'test_image_' + str(file_num) + '.png'
-	
+
 	# read image using opencv
 	img = cv2.imread(img_file_path)
-	
+
 	print('\n============================================')
 	print('\nFor test_image_' + str(file_num) + '.png')
-	
+
 	# detect shape properties from image
 	detected_shapes = detect_shapes(img)
 	print(detected_shapes)
-	
+
 	# display image with labeled shapes
 	img = get_labeled_image(img, detected_shapes)
 	cv2.imshow("labeled_image", img)
 	cv2.waitKey(2000)
 	cv2.destroyAllWindows()
-	
+
 	choice = input('\nDo you want to run your script on all test images ? => "y" or "n": ')
-	
+
 	if choice == 'y':
 
 		for file_num in range(1, 16):
-			
+
 			# path to test image file
 			img_file_path = img_dir_path + 'test_image_' + str(file_num) + '.png'
-			
+
 			# read image using opencv
 			img = cv2.imread(img_file_path)
-	
+
 			print('\n============================================')
 			print('\nFor test_image_' + str(file_num) + '.png')
-			
+
 			# detect shape properties from image
 			detected_shapes = detect_shapes(img)
 			print(detected_shapes)
-			
+
 			# display image with labeled shapes
 			img = get_labeled_image(img, detected_shapes)
 			cv2.imshow("labeled_image", img)
